b1f8a0f51ecfed83732ab61419fa6ea6a480087e
Steven Rdz <stevenrodriguezm97@gmail.com>
2022-12-03 23:31:22 -0500

    CSRF y Tipos - srodriguez03122022

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -7,13 +7,27 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+# Importación de librerias - Form
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 class PostType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('body')
+            ->add('title', TextType::class,[
+                'label' => 'Titulo de la Publicación',
+                'help' => 'Piensa en el SEO ¿Cómo buscaria en google?'
+            ])
+            ->add('body', TextareaType::class,[
+                'label' => 'Contenido',
+                'attr' => ['rows' => 9, 'class' => 'bg-light'] 
+            ])
+            ->add('Enviar', SubmitType::class,[
+                'attr' => ['class' => 'btn-dark'] 
+            ])
         ;
     }
 
@@ -21,6 +35,7 @@ class PostType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Post::class,
+            #'csrf_protection' => false
         ]);
     }
 }
